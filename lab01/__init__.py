dc8121ff86c6f9ad236fc64720c5d00172570f10
Шестаков Антон Владимирович <avshestakov@sberbank.ru>
2021-10-15 04:44:55 +0300

    add plot

--- lab01/__init__.py
+++ lab01/__init__.py
@@ -1,5 +1,11 @@
 import sys
+import numpy as np
 from util import func1
+
+def init_xy(N):
+    x_arr = np.zeros(N)
+    y_arr = np.zeros(N)
+    return x_arr, y_arr
 
 def uniformSearchMethod(N=1, a=0, b=1):
     """
@@ -12,7 +18,11 @@ def uniformSearchMethod(N=1, a=0, b=1):
     b : интервал ДО
     """
 
-    print("Метод равномерного перебора")
+    name = "Метод равномерного перебора"
+    print(name)
+    x_arr, y_arr = init_xy(N)
+    v_x = 0
+    v_y = 0
     step = (b - a) / N
     f_min = sys.maxsize
     for i in range(1, N+1, 1):
@@ -20,10 +30,26 @@ def uniformSearchMethod(N=1, a=0, b=1):
         f_val = func1(x)
         if (f_val < f_min):
             f_min = f_val
+            v_x = x
+            v_y = f_min
+
+        x_arr[i-1] = x
+        y_arr[i-1] = f_val
+
     print("Результат: v =", f_min)
+    return x_arr, y_arr, v_x, v_y, name
 
+def dichotomyMethod(N=1, a=0, b=1):
+    """
+    Метод дихотомии
 
+    Параметры
+    ---------
+    N : количество отрезков
+    a : интервал ОТ
+    b : интервал ДО
+    """
 
-
+    print("Метод дихотомии")
 
 
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import matplotlib.pyplot as plt
 from lab01 import uniformSearchMethod as usm
 
 if __name__ == '__main__':
-    usm(N=5, a=-5, b=5)
+    plt.rcParams['figure.figsize'] = [15, 5]
+    x_arr, y_arr, x, y, name = usm(N=5, a=-5, b=5)
+    plt.subplot(2, 4, 1)
+    plt.title(name)
+    plt.plot(x_arr, y_arr)
+    plt.scatter(x, y, color="red")
+    plt.show()
